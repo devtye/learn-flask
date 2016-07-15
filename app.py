2d36018735e40251f0b31be636d148afbbcb165d
Tye Wijesekara <twijesekar@mgmresorts.com>
2016-07-15 15:18:53 -0700

    ORM for flask, Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,45 +1,59 @@
 from flask import Flask , render_template, redirect, url_for, request, session, flash, g
+from flask_sqlalchemy import SQLAlchemy
 from functools import wraps
-import sqlite3
+
+#manual db import
+#import sqlite3
 
 app = Flask(__name__)
 
 app.secret_key = "devill may cry"
 
-app.database = "sample.db"
+#app.database = "sample.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
 
+#adds significant overhead and will be disabled by default in the future.  Set it to True to suppress this warning.
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+
+from models import *
+
+
+#SQLAlchemy OBJ
+db = SQLAlchemy(app)
+
+#password fucntion
 def login_required(f):
     @wraps(f)
     def wrap(*args, **kwargs):
         if 'logged_in'in session:
             return f(*args, **kwargs)
         else:
-            flash('You need to log in first.')
+            flash('You need to login first.')
             return redirect(url_for('login'))
     return wrap
 
-
+#index-  the landing route
 @app.route('/')
 @login_required
 def home():
-    g.db = connect_db()
-    cur = g.db.execute ('select * from posts')
-    posts = [dict(title=row[0], description=row[1]) for row in cur.fetchall()]
-    g.db.close()
+    posts = db.session.query(BlogPost).all()
     return render_template("index.html", posts=posts)
 
 
+#other page route
 @app.route('/smartsheet_demo')
 def smartsheet_demo():
     return render_template("smartsheet_demo.html")
 
 
+#app page route
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error= None
     if request.method == 'POST':
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid credentials. Please try again.'
+            error = 'Invalid Credentials. Please try again.'
         else:
             session['logged_in'] = True
             flash('You were just logged in.')
@@ -47,6 +61,7 @@ def login():
     return render_template('login.html', error=error)
 
 
+#logout page route
 @app.route('/logout')
 @login_required
 def logout():
@@ -54,8 +69,10 @@ def logout():
     flash('You were just logged out.')
     return redirect(url_for('home'))
 
+
+#database connetion route
 def connect_db():
-    return sqlite3.connect(app.database)
+    return sqlite3.connect('posts.db')
 
 if __name__ ==  '__main__':
     app.run(debug=True)
